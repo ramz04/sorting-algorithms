969dea9afa29e2a207413eed4bd89804b5ee29ba
Ramz04 <55752850+ramz04@users.noreply.github.com>
2026-09-01 22:38:59 +0000

    boot.dev DSA: sorting algorithms - merge sort

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-def bubble_sort(nums: list[int]) -> list[int]:
-    swap = True
-    end = len(nums)
+def merge_sort(nums: list[int]) -> list[int]:
+    if len(nums) < 2:
+        return nums
 
-    while swap == True:
-        swap = False
-        
-        for i in range(end - 1):
-            i = i + 1
-            if nums[i -1] > nums[i]:
-                x = nums[i]
-                nums[i] = nums[i - 1]
-                nums[i - 1] = x
-                swap = True
-        end -= 1
+    middle = len(nums) // 2
 
-    return nums
-                
+    left_half_nums = nums[:middle]
+    right_half_nums = nums[middle:]
+
+    left_half_nums = merge_sort(left_half_nums)
+    right_half_nums = merge_sort(right_half_nums)
+
+    return merge(left_half_nums, right_half_nums)
+
+
+def merge(first: list[int], second: list[int]) -> list[int]:
+    final = []
+    i, j = 0, 0
+
+    while i < len(first) and j < len(second):
+        if first[i] <= second[j]:
+            final.append(first[i])
+            i += 1
+        else:
+            final.append(second[j])
+            j += 1
+
+    final += first[i:]
+    final += second[j:]
+
+    return final
--- main_test.py
+++ main_test.py
@@ -1,29 +1,39 @@
+import time
+
 import pytest
-from main import bubble_sort
+from main import merge_sort
 
 run_cases = [
-    pytest.param([5, 7, 3, 6, 8], [3, 5, 6, 7, 8]),
-    pytest.param([2, 1], [1, 2]),
+    pytest.param([3, 2, 1], [1, 2, 3]),
+    pytest.param([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),
 ]
 
 submit_cases = [
     pytest.param([], [], marks=pytest.mark.submit),
-    pytest.param([1], [1], marks=pytest.mark.submit),
-    pytest.param([1, 5, -3, 2, 4], [-3, 1, 2, 4, 5], marks=pytest.mark.submit),
+    pytest.param([7], [7], marks=pytest.mark.submit),
+    pytest.param([4, -7, 1, 0, 5], [-7, 0, 1, 4, 5], marks=pytest.mark.submit),
     pytest.param(
-        [9, 8, 7, 6, 5, 4, 3, 2, 1],
-        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
         marks=pytest.mark.submit,
     ),
-    pytest.param([1, 3, 2, 5, 4], [1, 2, 3, 4, 5], marks=pytest.mark.submit),
+    pytest.param([1, 1, 1, 1, 1], [1, 1, 1, 1, 1], marks=pytest.mark.submit),
 ]
 
 
 @pytest.mark.parametrize(("input1", "expected_output"), run_cases + submit_cases)
-def test_bubble_sort(input1, expected_output):
+def test_merge_sort(input1, expected_output):
     print("\n---------------------------------")
-    print(f"Input:\n * {input1}")
+    print(f"Input: {input1}")
     print(f"Expected: {expected_output}")
-    result = bubble_sort(input1)
-    print(f"Actual:   {result}")
+    start = time.time()
+    result = merge_sort(input1)
+    elapsed = time.time() - start
+    timeout = 1.00
+    if elapsed < timeout:
+        print(f"test completed in less than {timeout * 1000} milliseconds!")
+    else:
+        print(f"test took longer than {timeout * 1000} milliseconds!")
+    print(f"Actual: {result}")
+    assert elapsed < timeout
     assert result == expected_output
