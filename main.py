4cf0b56db1c71637d70804ea7f7844cc2939d9d3
Ramz04 <55752850+ramz04@users.noreply.github.com>
2026-09-01 16:51:21 +0000

    boot.dev DSA: sorting algorithms - bubble sort

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-class Influencer:
-    def __init__(self, num_selfies: int, num_bio_links: int) -> None:
-        self.num_selfies = num_selfies
-        self.num_bio_links = num_bio_links
+def bubble_sort(nums: list[int]) -> list[int]:
+    swap = True
+    end = len(nums)
 
-    def __repr__(self) -> str:
-        return f"({self.num_selfies}, {self.num_bio_links})"
+    while swap == True:
+        swap = False
+        
+        for i in range(end - 1):
+            i = i + 1
+            if nums[i -1] > nums[i]:
+                x = nums[i]
+                nums[i] = nums[i - 1]
+                nums[i - 1] = x
+                swap = True
+        end -= 1
 
-
-# dont touch above this line
-
-
-def vanity(influencer: Influencer) -> int:
-    return (influencer.num_bio_links * 5) + influencer.num_selfies
-
-
-def vanity_sort(influencers: list[Influencer]) -> list[Influencer]:
-    return sorted(influencers, key=vanity, reverse=False)
+    return nums
+                
--- main_test.py
+++ main_test.py
@@ -1,34 +1,29 @@
 import pytest
-from main import Influencer, vanity_sort
-
-theprimeagen = Influencer(100, 1)
-pokimane = Influencer(800, 2)
-spambot = Influencer(0, 200)
-lane = Influencer(10, 2)
-badcop = Influencer(1, 2)
+from main import bubble_sort
 
 run_cases = [
-    pytest.param([badcop, lane], [badcop, lane]),
-    pytest.param([lane, badcop, pokimane], [badcop, lane, pokimane]),
-    pytest.param([spambot, theprimeagen], [theprimeagen, spambot]),
+    pytest.param([5, 7, 3, 6, 8], [3, 5, 6, 7, 8]),
+    pytest.param([2, 1], [1, 2]),
 ]
 
 submit_cases = [
     pytest.param([], [], marks=pytest.mark.submit),
-    pytest.param([lane], [lane], marks=pytest.mark.submit),
+    pytest.param([1], [1], marks=pytest.mark.submit),
+    pytest.param([1, 5, -3, 2, 4], [-3, 1, 2, 4, 5], marks=pytest.mark.submit),
     pytest.param(
-        [pokimane, theprimeagen, spambot, badcop, lane],
-        [badcop, lane, theprimeagen, pokimane, spambot],
+        [9, 8, 7, 6, 5, 4, 3, 2, 1],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
         marks=pytest.mark.submit,
     ),
+    pytest.param([1, 3, 2, 5, 4], [1, 2, 3, 4, 5], marks=pytest.mark.submit),
 ]
 
 
 @pytest.mark.parametrize(("input1", "expected_output"), run_cases + submit_cases)
-def test_vanity_sort(input1, expected_output):
+def test_bubble_sort(input1, expected_output):
     print("\n---------------------------------")
     print(f"Input:\n * {input1}")
     print(f"Expected: {expected_output}")
-    result = vanity_sort(input1)
+    result = bubble_sort(input1)
     print(f"Actual:   {result}")
     assert result == expected_output
