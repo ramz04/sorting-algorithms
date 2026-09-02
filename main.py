af8ae694dfc8dac32cf3245abf536faec14554df
Ramz04 <55752850+ramz04@users.noreply.github.com>
2026-09-02 12:36:16 +0000

    boot.dev DSA: sorting algorithms - quick sort

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-def insertion_sort(nums: list[int]) -> list[int]:
-    for i in range(len(nums)):
-        j = i
+def quick_sort(nums: list[int], low: int, high: int) -> None:
+    if low < high:
+        middle = partition(nums, low, high)
+        quick_sort(nums, low, middle - 1)
+        quick_sort(nums, middle + 1, high)
 
-        while j > 0 and nums[j - 1] > nums[j]:
-            nums[j], nums[j - 1] = nums[j - 1], nums[j]
-            j -= 1
 
-    return nums
+def partition(nums: list[int], low: int, high: int) -> int:
+    pivot = nums[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if nums[j] < pivot:
+            i += 1
+            nums[i], nums[j] = nums[j], nums[i]
             
-        
-        
+    nums[i + 1], nums[high] = nums[high], nums[i + 1]
+    return i + 1
+    
--- main_test.py
+++ main_test.py
@@ -1,37 +1,41 @@
 import time
 
 import pytest
-from main import insertion_sort
+from main import quick_sort
 
 run_cases = [
-    pytest.param([4, 3, 2, 1], [1, 2, 3, 4]),
-    pytest.param([9, 5, -3, 7], [-3, 5, 7, 9]),
+    pytest.param([2, 1, 3], 0, 2, [1, 2, 3]),
+    pytest.param([9, 6, 2, 1, 8, 7], 0, 5, [1, 2, 6, 7, 8, 9]),
 ]
 
 submit_cases = [
-    pytest.param([], [], marks=pytest.mark.submit),
-    pytest.param([1], [1], marks=pytest.mark.submit),
-    pytest.param([5, 3, 4, 1, 2], [1, 2, 3, 4, 5], marks=pytest.mark.submit),
+    pytest.param([], 0, -1, [], marks=pytest.mark.submit),
+    pytest.param([1], 0, 0, [1], marks=pytest.mark.submit),
+    pytest.param([1, 2, 3, 4, 5], 0, 4, [1, 2, 3, 4, 5], marks=pytest.mark.submit),
+    pytest.param([5, 4, 3, 2, 1], 0, 4, [1, 2, 3, 4, 5], marks=pytest.mark.submit),
     pytest.param(
-        [0, -2, -5, 3, 2, 1],
-        [-5, -2, 0, 1, 2, 3],
-        marks=pytest.mark.submit,
-    ),
-    pytest.param(
-        [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 1, 6, 4, 7, 3, 2, 8, 5, -9],
+        0,
+        9,
+        [-9, 0, 1, 2, 3, 4, 5, 6, 7, 8],
         marks=pytest.mark.submit,
     ),
 ]
 
 
-@pytest.mark.parametrize(("input1", "expected_output"), run_cases + submit_cases)
-def test_insertion_sort(input1, expected_output):
+@pytest.mark.parametrize(
+    ("input1", "input2", "input3", "expected_output"), run_cases + submit_cases
+)
+def test_quick_sort(input1, input2, input3, expected_output):
     print("\n---------------------------------")
-    print(f"Inputs: {input1}")
+    print("Inputs:")
+    print(f" * nums: {input1}")
+    print(f" * low: {input2}")
+    print(f" * high: {input3}")
     print(f"Expected: {expected_output}")
     start = time.time()
-    result = insertion_sort(input1)
+    result = input1.copy()
+    quick_sort(result, input2, input3)
     elapsed = time.time() - start
     timeout = 1.00
     if elapsed < timeout:
